c1bff7f9b35f20fa7465b059dc4af5e9bb836904
faith <whh306318848@aliyun.com>
2017-02-10 17:37:44 +0800

    创建项目
    1、创建入口文件
    2、编写自动加载函数

---
+++ core/imooc.php
@@ -0,0 +1,21 @@
+<?php
+namespace core;
+/**
+ * Created by PhpStorm.
+ * User: sailwish004
+ * Date: 2017/2/10
+ * Time: 16:46
+ */
+class imooc
+{
+    public static function run() {
+        p('ok');
+    }
+
+    /**
+     * 自动加载类库
+     */
+    public static function load() {
+
+    }
+}
---
+++ core/route.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sailwish004
+ * Date: 2017/2/10
+ * Time: 16:49
+ */
+
+namespace core;
+
+
+class route
+{
+
+}
