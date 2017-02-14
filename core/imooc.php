98725136f5005edafe3d85416b7e7d0e9fa504e3
faith <whh306318848@aliyun.com>
2017-02-14 16:13:41 +0800

    Revert "Revert "添加自动加载函数""
    This reverts commit 907dff887651d28d10f467cf573097a5bcf27801.

--- core/imooc.php
+++ core/imooc.php
@@ -8,14 +8,30 @@ namespace core;
  */
 class imooc
 {
+    public static $classMap = array();
     public static function run() {
         p('ok');
+        $route = new \core\route();
     }
 
     /**
      * 自动加载类库
+     * @param $class 自动引入的类的名称
      */
-    public static function load() {
-
+    public static function load($class) {
+        // 正常情况下创建一个对象应该是new \core\route();
+        // 函数传入的参数$class = '\core\route';
+        // 真正引入的路径IMOOC.'/core/route.php'
+        $class = str_replace('\\', '/', $class);
+        if (isset($classMap[$class])) {
+            return true;
+        }
+        $file = IMOOC.'/'.$class.'.php';
+        if (is_file($file)) {
+            include $file;
+            self::$classMap[$class] = $class;
+        }else {
+            return false;
+        }
     }
 }
